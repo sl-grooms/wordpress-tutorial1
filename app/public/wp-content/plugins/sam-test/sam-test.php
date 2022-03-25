bcb10abb00812fe9b743680fc389b3078b3c8ea4
samantha <sgrooms.webdev@gmail.com>
2022-03-25 14:00:18 -0600

    I added security code here

--- app/public/wp-content/plugins/sam-test/sam-test.php
+++ app/public/wp-content/plugins/sam-test/sam-test.php
@@ -12,3 +12,15 @@
  License: (open source license)
  Text Domain: sam-text
  */
+if ( ! defined( ' ABSPATH ') ) {
+    die;
+}
+
+defined (' ABSPATH ') or die ('You cannot access this');
+
+if ( ! function_exists( 'add_action' ) ) {
+    echo 'You cannot access this file';
+    exit;
+}
+
+//This page is here for security
