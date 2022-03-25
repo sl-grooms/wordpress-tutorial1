c69ed15224170d5d27056d883edef78c2effd8fd
samantha <sgrooms.webdev@gmail.com>
2022-03-25 13:48:28 -0600

    Creating a plugin! Here is the start https://www.youtube.com/watch?v=Z7QfH-s-15s&list=TLPQMjUwMzIwMjKLo-CRkYhWMw&index=2

---
+++ app/public/wp-content/plugins/sam-test/sam-test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Sam-test
+ */
+/*
+ Plugin Name: Sam Test
+ Plugin URI: http://
+ Description: This is my first attempt at creating a custom plugin
+ Version: 1.0.0
+ Author: Samantha Grooms
+ Author URI: http://
+ License: (open source license)
+ Text Domain: sam-text
+ */
