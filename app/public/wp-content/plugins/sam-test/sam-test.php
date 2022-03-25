0f646a024f429e7b14bb01a62e3f8dec920cccb4
samantha <sgrooms.webdev@gmail.com>
2022-03-25 14:25:07 -0600

    I got the plugin activated on my test site and added some code https://www.youtube.com/watch?v=34HJlgkxieg&list=TLPQMjUwMzIwMjKLo-CRkYhWMw&index=4

--- app/public/wp-content/plugins/sam-test/sam-test.php
+++ app/public/wp-content/plugins/sam-test/sam-test.php
@@ -12,15 +12,34 @@
  License: (open source license)
  Text Domain: sam-text
  */
-if ( ! defined( ' ABSPATH ') ) {
-    die;
-}
+//if ( ! defined( ' ABSPATH ') ) {
+//    die;
+//}
 
 defined (' ABSPATH ') or die ('You cannot access this');
 
-if ( ! function_exists( 'add_action' ) ) {
-    echo 'You cannot access this file';
-    exit;
+//if ( ! function_exists( 'add_action' ) ) {
+//    echo 'You cannot access this file';
+//    exit;
+//}
+
+//The code above is here for security
+
+class samTest
+{
+    function __construct($string)
+    {
+        echo $string;
+    }
 }
 
-//This page is here for security
+//if ( class_exists ( 'samTest') ) {
+//    $sam_test = new samTest('sam test is working');
+//}
+//
+//function customFunction($arg) {
+//    echo $arg;
+//}
+
+//customFunction('this is an argument to echo');
+
