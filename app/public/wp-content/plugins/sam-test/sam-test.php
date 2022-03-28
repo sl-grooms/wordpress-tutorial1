8a07c9b8511eedbf381ef8287007d526cc50e6a6
samantha <sgrooms.webdev@gmail.com>
2022-03-28 13:39:07 -0600

    Figured out activate/deactivate hooks

--- app/public/wp-content/plugins/sam-test/sam-test.php
+++ app/public/wp-content/plugins/sam-test/sam-test.php
@@ -16,7 +16,8 @@
 //    die;
 //}
 
-defined (' ABSPATH ') or die ('You cannot access this');
+//This is denying me access to the site
+//defined (' ABSPATH ') or die ('You cannot access this');
 
 //if ( ! function_exists( 'add_action' ) ) {
 //    echo 'You cannot access this file';
@@ -27,19 +28,34 @@ defined (' ABSPATH ') or die ('You cannot access this');
 
 class samTest
 {
-    function __construct($string)
-    {
-        echo $string;
+    function activate() {
+        //generate a custom post type (CPT)
+        //flush rewrite rules
+        }
+    function deactivate() {
+        //flush rewrite rules
+        }
+    function uninstall() {
+        //delete CPT
+        //delete all the plugin data from the DB
     }
 }
 
-//if ( class_exists ( 'samTest') ) {
-//    $sam_test = new samTest('sam test is working');
-//}
-//
+if ( class_exists ( 'samTest') ) {
+    $sam_test = new samTest();
+}
+
+// activation
+register_activation_hook( __FILE__, array( $sam_test, 'activate') ); // only in this file. pass function name into array activate method inside sam_test
+//deactivation
+
+register_deactivation_hook( __FILE__, array( $sam_test, 'activate') ); // same thing as above but deactivates hook
+//uninstall
+
+
 //function customFunction($arg) {
 //    echo $arg;
 //}
-
+//
 //customFunction('this is an argument to echo');
 
